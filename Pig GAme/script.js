66b2f3634b1c66ba6dd19b76cd194e187c620efe
Felix <cmekusfelix8@gmail.com>
2022-08-30 20:57:37 +0100

    game completed

--- Pig GAme/script.js
+++ Pig GAme/script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const scoreReset = document.querySelectorAll('.score');
+const currentScoreReset = document.querySelector('.current-score');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
@@ -18,46 +20,111 @@ const player1El = document.querySelector('.player--1');
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-// images array
-// const imagesArray = [
-//   'dice-1.png',
-//   'dice-2.png',
-//   'dice-3.png',
-//   'dice-4.png',
-//   'dice-5.png',
-//   'dice-6.png',
-// ];
-let currentScore = 0;
-let activePlayer = 0;
- 
-const displayImage = () => {
-  // generate a random dice roll
-  const dice = Math.floor(Math.random() * 6) + 1;
+let currentScore, activePlayer, scores, playing;
+
+
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    document.getElementById(`current--0`).textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
   
-  // display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`
+     diceEl.classList.add('hidden');
+    player1El.classList.remove('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+}
+init();
+
+const switchPlayer = () => {
+     document.getElementById(`current--${activePlayer}`).textContent = 0;
+     // we check and return a new active player everytime the score is 1
+     activePlayer = activePlayer === 0 ? 1 : 0;
+     currentScore = 0;
+     player0El.classList.toggle('player--active');
+     player1El.classList.toggle('player--active');
+}
+
+const displayImage = () => {
+  if (playing) {
+    // generate a random dice roll
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
     // dice.src = imagesArray[diceNumber];
     // console.log(diceNumber);
 
-  // check for rolled 1
+    // check for rolled 1
     if (dice !== 1) {
-       currentScore += dice;
-       document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-       document.getElementById(`current--${activePlayer}`).textContent = 0; 
-      // we check and return a new active player everytime the score is 1
-      activePlayer = activePlayer === 0 ? 1 : 0;
-      currentScore = 0;
-      player0El.classList.toggle('player--active');
-      player1El.classList.toggle('player--active');
-       // swithUser();
+      switchPlayer();
+      // swithUser();
     }
+  }
 }
 
 
 
 // rolling dice
 btnRoll.addEventListener('click', displayImage);
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    
+    //  1. Add current score to active player's score;
+    scores[activePlayer] = scores[activePlayer] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+  
+    // document.getElementById
+    //  2. check if score is at least 100
+    if (scores[activePlayer] >= 10) {
+      //  finish the game
+      playing = false
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+       diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
+  }
+  
+
+  // switch to next player
+  
+ })
+
+// const resetGame = (e) => {
+//   playing = true
+//   e.textContent = 0;
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.remove('player--winner');
+  
+
+//  }
+
+// reset game
+btnNew.addEventListener('click', init);
+
+
+
+
+
+
+
+
+
+
 
 
